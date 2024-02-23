b1e3c8bbc048e9f2232b8afe6886912701e7b6ee
Jayant Chhangani <jayantc20@gmail.com>
2024-02-23 23:32:32 +0530

    Delete dist directory

--- dist/controllers/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getIndex = void 0;
-const getIndex = (req, res) => {
-    res.send('Hello, from the controller!');
-};
-exports.getIndex = getIndex;
--- dist/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// src/routes/indexRoutes.ts
-const express_1 = require("express");
-const index_1 = require("../controllers/index");
-const router = (0, express_1.Router)();
-router.get('/', (req, res) => (0, index_1.getIndex)(req, res));
-exports.default = router;
